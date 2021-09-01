ba875d140c35006469f835885ec60bf9c2461345
Rupam Sau <rupamsau0010@users.noreply.github.com>
2021-09-02 00:21:36 +0530

    product Schema ready

---
+++ models/products.js
@@ -0,0 +1,31 @@
+// Import Depandencies
+const mongoose = require("mongoose")
+
+// Import Local depandencies
+
+// Create a Schema
+const productSchema = new mongoose.Schema({
+    productName: {
+        type: String,
+        required: true
+    },
+    productTitle: {
+        type: String,
+        required: true
+    },
+    productPrice: {
+        type: Number,
+        required: true
+    },
+    productImgUrl: {
+        type: String,
+    },
+    productCreatedAt: {
+        type: Date,
+        default: new Date()
+    }
+})
+
+// Export the Module
+const Product = mongoose.model("products", productSchema)
+module.exports = Product
