c137bdfbed26fb235faca4f23c5e3016d2b4ebe3
Rupam Sau <rupamsau0010@users.noreply.github.com>
2021-09-02 10:43:14 +0530

    Searching Product Complete

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,4 +1,5 @@
 // Import depandencies
+const stringSimilarity = require("string-similarity")
 
 // Import Loacl depandencies
 const Product = require("../models/products")
@@ -10,13 +11,67 @@ module.exports.getAllProducts_get = async(req, res) => {
         if (err1) {
             res.json({
                 status: "failure", 
-                data: "Internal Problem. Please Try Again."
+                payload: "Internal Problem. Please Try Again."
             })
         } else {
             res.json({
                 status: "success",
-                data: data1
+                payload: data1
             })
         }
+    })
+}
+
+// Enter New Product
+module.exports.enterNewProducts_post = async(req, res) => {
+    const product = new Product({
+        productName: req.body.productName,
+        companyName: req.body.companyName,
+        productTitle: req.body.productTitle,
+        productPrice: req.body.productPrice,
+        productImgUrl: req.body.productImgUrl
+    })
+
+    product.save((err1, data1) => {
+        if (err1) {
+            res.json({
+                status: "failure",
+                payload: "Internal Error Occured. Please Try Again."
+            })
+        } else {
+            res.json({
+                status: "succuess",
+                payload: data1
+            })
+        }
+    })
+}
+
+// Search a Product
+module.exports.searchProduct_get = async(req, res) => {
+    // Get the Search Data
+    const searchGiven = req.body.searchGiven
+    
+    const probability = (val, index) => {
+        // console.log(val.tagline);
+        // console.log(searchGiven);
+        const prob = stringSimilarity.compareTwoStrings(searchGiven.toLowerCase(), val.productName.toLowerCase());
+        return {data: val, value: prob}
+    }
+
+    // Query on the database
+    const products = await Product.find({}).sort({ _id: -1 })
+
+    productArray = products.map(probability)
+
+    productArray.sort(function(first, second){
+        return second.value - first.value
+    })
+
+    const finalProductArray = productArray.filter(element => element.value >= 0.3)
+
+    res.json({
+        status: "success",
+        payload: finalProductArray
     })
 }
--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -8,5 +8,11 @@ const productControllers = require("../controllers/productControllers")
 // Get All Products Route
 router.get("/getallproducts", productControllers.getAllProducts_get)
 
+// Create a New Product
+router.post("/enterproduct", productControllers.enterNewProducts_post)
+
+// Search a product
+router.get("/searchproduct", productControllers.searchProduct_get)
+
 // Export the Module
 module.exports = router
