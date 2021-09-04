a03314d55480075f54133844adc712ab88b75c8c
Rupam Sau <rupamsau0010@users.noreply.github.com>
2021-09-04 12:20:17 +0530

    prepare for deploy -6

--- middlewares/authMiddlewares.js
+++ middlewares/authMiddlewares.js
@@ -4,12 +4,13 @@ const jwt = require("jsonwebtoken");
 // Middleware for Protecting routes...
 const requireAuth = (req, res, next) => {
     try {
-        const token = req.cookies.jwt;
+        const token = req.body.jwt
+        console.log(token);
         const decodeToken = jwt.verify(token, process.env.JWT_SECRET)
-        req.userData = decodeToken
         console.log(decodeToken);
         next()
     } catch (error) {
+        // console.log(error);
         return res.json({
             status: "invalid",
             payload: "You are not authorized to post a product. Please consider login/signup"
